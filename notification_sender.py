70950b28229036394b255b93659571baff12d20e
dylanbyrnecarr <x00155165@mytudublin.ie>
2020-10-28 10:46:29 +0000

    changed password to fake

--- notification_sender.py
+++ notification_sender.py
@@ -3,7 +3,7 @@ recipients = ['dylanbyrnecarr@gmail.com', 'Mattlynch1999@gmail.com']
 
 def send_push_notification_success():
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login('dylanbyrnecarr@gmail.com', 'Homestuck42')
+    server.login('dylanbyrnecarr@gmail.com', 'PASSWORD')
     server.sendmail("dylanbyrnecarr@gmail.com", recipients, "Next Week Schedules are up")
     server.quit
 
